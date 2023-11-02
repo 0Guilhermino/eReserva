e7aeba219d55d3251bb61cb43882a83ca907de8b
Angelo Guilhermino <angeloguilhermino@gmail.com>
2023-11-02 16:19:48 -0300

    feat(eReserva):add primeng to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {CommonPageModule} from "./pages/common-page.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    CommonPageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
